79c6c4547ce103d054bd30e3a38b8e173aa7a94b
Karan Jakhar <karanjakhar49@gmail.com>
2021-08-12 11:47:51 +0530

    loading weigth only once

--- yolov5_tflite_video_inference.py
+++ yolov5_tflite_video_inference.py
@@ -65,7 +65,9 @@ def detect_video(weights,video_filepath,img_size,conf_thres,iou_thres):
 
         
         out.write(frame)
-        print('FPS:',no_of_frames/(time.time()-start_time))
+        end_time = time.time()
+        print('FPS:',1/(end_time-start_time))
+        start_time = end_time
     out.release()
 
 
